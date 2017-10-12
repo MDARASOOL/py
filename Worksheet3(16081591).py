0669bac7d67865ce3bc5d70da78b62bc676d5dc2
Mohammed Aftabuddin Rasool <aftabuddinra@live.com>
2017-10-12 20:10:37 +0100

    Add files via upload

---
+++ Worksheet3(16081591).py
@@ -0,0 +1,30 @@
+# WORKSHEET 3 GUI-FOR GRGOBOT using tiNKTER
+# Not Python 2 Compactible
+
+
+import tkinter as tk
+from grobot import*
+
+bill=NewRobot("bill")
+
+rcApp=tk.Tk()
+rcApp.title(" Grid Robot-Control ")  # Title of the box
+
+# to create buttons and place in tha GUI
+
+rstButton = tk.Button( rcApp, text="Restart" , command = bill.init)
+rstButton.pack( side=tk.TOP)
+
+
+rgtButton =  tk.Button( rcApp, text="Right" , command = bill.right)
+rgtButton.pack( side=tk.RIGHT)
+
+
+lftButton = tk.Button( rcApp , text="Left" , command= bill.left)
+lftButton.pack( side=tk.LEFT)
+
+
+fwButton = tk.Button(rcApp , text="Forward" , command= bill.forward)
+fwButton.pack()
+
+rcApp.mainloop()
